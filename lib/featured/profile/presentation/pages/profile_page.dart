ac753423d338d7e72b43cba698ef9a15064a7d48
Trysor-llachar <trysorllachar@gmail.com>
2024-12-03 14:40:11 +0700

    unstable version form here 3

--- lib/featured/profile/presentation/pages/profile_page.dart
+++ lib/featured/profile/presentation/pages/profile_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:social_network_lite/featured/auth/domain/entities/app_user.dart';
 import 'package:social_network_lite/featured/auth/presentation/cubits/auth_cubit.dart';
+import 'package:social_network_lite/featured/chat/presentation/cubits/chat_cubit.dart';
 import 'package:social_network_lite/featured/chat/presentation/pages/chat_page.dart';
 import 'package:social_network_lite/featured/post/presentation/cubits/post_cubit.dart';
 import 'package:social_network_lite/featured/post/presentation/cubits/post_states.dart';
@@ -116,15 +117,22 @@ class _ProfilePageState extends State<ProfilePage> {
                 // start chat button
                 if (!isOwnPost)
                   IconButton(
-                    onPressed: () {
+                    onPressed: ()async {
+                      final chatCubit = context.read<ChatCubit>();
+                      await chatCubit.createChat(currentUser!.uid,
+                          widget.uid); // Gọi createChat từ ChatCubit
                       Navigator.push(
                         context,
                         MaterialPageRoute(
-                          builder: (context) =>
-                              ChatPage(myId: currentUser!.uid, friendId: user.uid, friendName: user.name, chatDocId: '',),
+                          builder: (context) => ChatPage(
+                            myId: currentUser!.uid,
+                            friendId: user.uid,
+                            friendName: user.name,
+                            chatDocId: ChatCubit.,
+                          ),
                         ),
                       );
-                    } ,
+                    },
                     icon: const Icon(Icons.chat),
                   )
               ],
@@ -196,7 +204,6 @@ class _ProfilePageState extends State<ProfilePage> {
                       isFollowing: user.followers.contains(currentUser!.uid)),
 
                 const SizedBox(height: 25),
-
 
                 //bio box
                 Padding(
