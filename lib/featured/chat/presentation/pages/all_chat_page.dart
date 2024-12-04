f6c5cc3178f648b2b86beb0a0ee28e06e2d48ad9
Trysor-llachar <trysorllachar@gmail.com>
2024-12-04 17:05:23 +0700

    unstable version form here 1.4

--- lib/featured/chat/presentation/pages/all_chat_page.dart
+++ lib/featured/chat/presentation/pages/all_chat_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:social_network_lite/featured/auth/presentation/cubits/auth_cubit.dart';
 import 'package:social_network_lite/featured/chat/presentation/component/chat_tile.dart';
 import 'package:social_network_lite/featured/chat/presentation/cubits/chat_cubit.dart';
 import 'package:social_network_lite/featured/chat/presentation/cubits/chat_states.dart';
+
+import '../../../auth/domain/entities/app_user.dart';
 
 class AllChatPage extends StatefulWidget {
   final String uid;
@@ -14,38 +17,45 @@ class AllChatPage extends StatefulWidget {
 }
 
 class _AllChatPageState extends State<AllChatPage> {
+  late final authCubit = context.read<AuthCubit>();
+  late AppUser? currentUser = authCubit.currentUser;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: BlocBuilder<ChatCubit, ChatStates>(
-          builder: (context, state) {
-            //loading
-            if (state is ChatLoading) {
-              return const Center(child: CircularProgressIndicator(),);
-            }
+      body: BlocBuilder<ChatCubit, ChatStates>(builder: (context, state) {
+        //loading
+        if (state is ChatLoading) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
 
-            //loaded
-            else if (state is AllChatLoaded) {
-              final allChats = state.chats;
+        //loaded
+        else if (state is AllChatLoaded) {
+          final allChats = state.chats;
 
-              if (allChats.isEmpty) {
-                return const Center(child: Text("Start your chat now!  =w=  "),
-                );
-              }
-              return ListView.builder(
-                itemCount:allChats.length,
-                itemBuilder: (context, index) {
-                  //get indivitual chat UwU~
-                  final chat = allChats[index];
-
-                  // image
-                  return ChatTile(chat: chat)
-                },
-              );
-            }
+          if (allChats.isEmpty) {
+            return const Center(
+              child: Text("Start your chat now!  =w=  "),
+            );
           }
-      ),
+          return ListView.builder(
+            itemCount: allChats.length,
+            itemBuilder: (context, index) {
+              //get indivitual chat UwU~
+              final chat = allChats[index];
+
+              // image
+              return ChatTile(
+                chat: chat,
+                curUid: currentUser!.uid,
+              );
+            },
+          );
+        }
+      }),
     );
   }
 }
