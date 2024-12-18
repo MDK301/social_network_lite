8e250d7b20e8e03642141aec9a750ad7b3efb937
Trysor-llachar <trysorllachar@gmail.com>
2024-12-18 11:50:43 +0700

    unstable version form here 6.0 ok

--- lib/featured/chat/domain/entities/chat.dart
+++ lib/featured/chat/domain/entities/chat.dart
@@ -29,16 +29,6 @@ class Chat {
     };
   }
 
-  // convert json -> chat
-  // factory Chat.fromJson(Map<String, dynamic> json) {
-  //   return Chat(
-  //     id: json['id'],
-  //     lastMessenger: json['lastMessenger'],
-  //     lastMessengerTime:json['lastMessageTimestamp'] != null ? (json['lastMessengerTime'] as Timestamp).toDate():  ,
-  //     participate: List<String>.from(json['participate']),
-  //     unread: List<String>.from(json['unread'] ?? []),
-  //   );
-  // }
   factory Chat.fromJson(Map<String, dynamic> json) {
     return Chat(
       id: json['id'] as String,
@@ -52,24 +42,3 @@ class Chat {
     );
   }
 }
-// // Từ Firestore Document -> Chat
-// factory Chat.fromFirestore(DocumentSnapshot doc) {
-//   Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
-//   return Chat(
-//     id: doc.id,
-//     lastMessenger: data['lastMessenger'] ?? '',
-//     lastMessengerTime: (data['lastMessengerTime'] as Timestamp).toDate(),
-//     participate: List<String>.from(data['participate'] ?? []),
-//     unread: List<String>.from(data['unread'] ?? []),
-//   );
-// }
-//
-// // Từ Chat -> Map để lưu vào Firestore
-// Map<String, dynamic> toFirestore() {
-//   return {
-//     'lastMessenger': lastMessenger,
-//     'lastMessengerTime': lastMessengerTime,
-//     'participate': participate,
-//     'unread': unread,
-//   };
-// }
