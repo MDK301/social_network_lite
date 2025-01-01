745aa016205c05ef60a368868cc9f3e79e44be6b
Trysor-llachar <trysorllachar@gmail.com>
2025-01-01 11:54:25 +0700

    unstable version form here 9.2 add blockList and emailPrivacy

--- lib/featured/profile/domain/entities/profile_user.dart
+++ lib/featured/profile/domain/entities/profile_user.dart
@@ -3,41 +3,49 @@ import 'package:social_network_lite/featured/auth/domain/entities/app_user.dart'
 class ProfileUser extends AppUser {
   final String bio;
   final String profileImageUrl;
+  final String? emailPrivacy;
   final List<String> followers;
   final List<String> following;
   final List<String>? friendlist;
   final List<String>? friendRequest;
+  final List<String>? blockList;
 
   ProfileUser({
     required super.uid,
     required super.email,
     required super.name,
     required this.bio,
+     this.emailPrivacy,
     required this.profileImageUrl,
     required this.followers,
     required this.following,
      this.friendlist,
      this.friendRequest,
+     this.blockList,
   });
 
   //method to update profile user
   ProfileUser copyWith({String? newBio,
     String? newprofileImageUrl,
+    String? newemailPrivacy,
     List<String>? newFollowers,
     List<String>? newFollowing,
     List<String>? newfriendlist,
     List<String>? newfriendRequest,
+    List<String>? newblockList,
   }) {
     return ProfileUser(
         uid: uid,
         email: email,
         name: name,
         bio: newBio ?? bio,
+        emailPrivacy: newemailPrivacy ?? emailPrivacy,
         profileImageUrl: newprofileImageUrl ?? profileImageUrl,
         followers: newFollowers ?? followers,
         following: newFollowing ?? following,
         friendlist: newfriendlist ?? friendlist,
         friendRequest: newfriendRequest ?? friendRequest,
+        blockList: newblockList ?? blockList,
     );
   }
 
@@ -48,11 +56,13 @@ class ProfileUser extends AppUser {
       'email': email,
       'name': name,
       'bio': bio,
+      'emailPrivacy': emailPrivacy,
       'profileImageUrl': profileImageUrl,
       'followers': followers,
       'following': following,
       'friendlist': friendlist,
       'friendRequest': friendRequest,
+      'blockList': blockList,
     };
   }
 
@@ -63,11 +73,13 @@ class ProfileUser extends AppUser {
       email: json['email'],
       name: json['name'],
       bio: json['bio'] ?? '',
+      emailPrivacy: json['emailPrivacy'] ?? '',
       profileImageUrl: json['profileImageUrl'] ?? '',
       followers: List<String>.from(json['followers'] ?? []),
       following: List<String>.from(json['following'] ?? []),
       friendlist: List<String>.from(json['friendlist'] ?? []),
       friendRequest: List<String>.from(json['friendRequest'] ?? []),
+      blockList: List<String>.from(json['blockList'] ?? []),
     );
   }
 }
